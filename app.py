cbfc02d302a4455736f860437f925cd22cd5c888
Maina2027 <mainakameta08@gmail.com>
2025-02-17 17:11:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import streamlit as st
 # Generate random time series data
 time_series = np.random.randn(100)
+
 if st.button("Test this"):
   # Plot the time series
   plt.plot(time_series)
